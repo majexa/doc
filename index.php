2ddfe1c554f443e0b6638f3bfcdf022a6164294d
masted <masted311@gmail.com>
2015-03-13 09:22:30 +0300

    Auto-commit on 13.03.2015 10:22:30

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 require __DIR__.'/init.php';
 
 define('TEMPLATE_DEBUG', true);
-define('PROJECT_PATH', __DIR__.'/site');
 require dirname(dirname(__DIR__)).'/ngn/init/web-standalone.php';
 Lib::addFolder(DOC_PATH.'/site/lib');
 Lib::addFolder(NGN_ENV_PATH.'/pm/lib');
