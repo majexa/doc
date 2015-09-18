95dd035954ae50d17e6d4d5f186b2137c16db100
masted <masted311@gmail.com>
2015-09-18 20:22:46 +0300

    ...

--- cron.bat
+++ cron.bat
@@ -1,2 +1,4 @@
-"C:\Program Files (x86)\Git\bin\sh.exe" -l "C:\www\refactor\ngn-env\projects\doc\commit.bat"
+"C:\Program Files (x86)\Git\bin\sh.exe" --login -i "C:\www\refactor\ngn-env\projects\doc\commit.bat"
+pause
+
 
