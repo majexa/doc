2080526af34ac6d3550e3eabf274985e03dd60d6
masted <masted311@gmail.com>
2015-09-21 20:36:51 +0300

    Auto-commit on 21.09.2015 20:36:51

--- cron.bat
+++ cron.bat
@@ -1,4 +1,4 @@
-"C:\Program Files (x86)\Git\bin\sh.exe" --login -i "C:\www\refactor\ngn-env\projects\doc\commit.bat"
+"C:\Program Files (x86)\Git\bin\sh.exe" --login -i --open-shell="C:\www\refactor\ngn-env\projects\doc" "C:\www\refactor\ngn-env\projects\doc\commit.bat"
 pause
 
 
