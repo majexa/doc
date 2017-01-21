426d6bdf0c41ffcd91067f542a37f0b87ed31102
Andrey-Yartsev <andrey.yartsev.g@gmail.com>
2017-01-21 15:50:43 +0300

    fix init

--- site/init.php
+++ site/init.php
@@ -1,5 +1,5 @@
 <?php
 
 Lib::addFolder(NGN_ENV_PATH.'/pm/lib');
-require_once NGN_ENV_PATH.'/ngn-cst/init.php';
+require_once NGN_ENV_PATH.'/ngn-cs/init.php';
 require __DIR__.'/lib/hyperlight/hyperlight.php';
